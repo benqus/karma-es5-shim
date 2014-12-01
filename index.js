0323e1e93c75ccf94506fd42fb92c580efadf56d
Bence Kormos <bence.kormos@hogarthww.com>
2014-12-01 17:59:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+var createPattern = function (pattern) {
+    return { pattern: pattern, included: true, watched: false };
+};
+
+var initShim = function (files) {
+    files.unshift(createPattern(path.resolve(require.resolve('es5-shim'))));
+};
+
+initShim.$inject = ['config.files'];
+module.exports = {
+    'framework:es5-shim': ['factory', initShim]
+};
